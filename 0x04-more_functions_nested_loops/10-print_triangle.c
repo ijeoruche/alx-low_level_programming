7555a89accc6ddb885729c8fe82a391aff6219df
ijeoruche <nwanzeij@gmail.com>
2022-12-15 23:44:57 -0800

    update

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -16,28 +16,18 @@ for (inc1 = 1; inc1 <= size; inc1++)
 {
 for ((inc2 = size - inc1); inc2 > 0; inc2--)
 {
-putchar(' ');
+_putchar(' ');
 }
 for (inc2 = 0; inc2 < inc1; inc2++)
 {
-putchar('#');
+_putchar('#');
 }
 if (inc1 == size)
 {
 continue;
 }
-putchar('\n');
-}
-}
-putchar('\n');
-}
-putchar(' ');
-}
-for (j = i; j <= i; j++)
-{
-_putchar('#');
-}
 _putchar('\n');
 }
 }
+_putchar('\n');
 }
