feaf1a49b7d56558eb896673812af04b95582ca2
ijeoruche <nwanzeij@gmail.com>
2022-12-22 05:14:50 -0800

    update

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcat - cancatennates two strings
+ * @dest: copy to
+ * @src: copy from
+ * Return: pointer to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int b;
+	int c;
+
+	b = 0;
+	while (dest[b] != '\0')
+{
+	b++;
+}
+c = 0;
+while (src[c]  != '\0')
+{
+	dest[b] = src[c];
+	b++;
+	c++;
+}
+dest[b] = '\0';
+return (dest);
+}
+
