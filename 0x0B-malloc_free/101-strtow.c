42b8c318a477ed3e0d2f8f582637801375a88346
ijeoruche <nwanzeij@gmail.com>
2023-01-10 05:53:49 -0800

    update

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -63,8 +63,8 @@ char **strtow(char *str)
 					malloc(sizeof(char) * (c + 1));
 			if (tmp == NULL)
 			return (NULL);
-					while (start < end)
-							*tmp++ = str[start++];
+	while (start < end)
+		*tmp++ = str[start++];
 							*tmp = '\0';
 							matrix[k] = tmp - c;
 							k++;
